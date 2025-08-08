c2d6e158f9cee03cdcba6bc50700cc74d7589893
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-08-08 14:21:34 -0700

    allocations

---
+++ allocations.go
@@ -0,0 +1,14 @@
+package ilpaddr
+
+const (
+	AllocationGlobal  = "g"
+	AllocationPrivate = "private"
+	AllocationExample = "example"
+	AllocationTest    = "test"
+	AllocationTest1   = "test1"
+	AllocationTest2   = "test2"
+	AllocationTest3   = "test3"
+	AllocationLocal   = "test1"
+	AllocationPeer    = "peer"
+	AllocationSelf    = "self"
+)
