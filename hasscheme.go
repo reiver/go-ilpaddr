668e5d627ffb392d915f1a52929240094ac70173
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-08-08 14:38:35 -0700

    allocation -> scheme

---
+++ hasscheme.go
@@ -0,0 +1,18 @@
+package ilpaddr
+
+import (
+	"strings"
+)
+
+// HasScheme returns true of the ILP Address (ilpAddr) has the specified scheme.
+func HasScheme(ilpAddr string, scheme string) bool {
+	if "" == scheme {
+		return false
+	}
+	if "" == ilpAddr {
+		return false
+	}
+
+	var prefix string = scheme + "."
+	return strings.HasPrefix(ilpAddr, prefix)
+}
--- schemes.go
+++ schemes.go
@@ -8,7 +8,7 @@ const (
 	SchemeTest1   = "test1"
 	SchemeTest2   = "test2"
 	SchemeTest3   = "test3"
-	SchemeLocal   = "test1"
+	SchemeLocal   = "local"
 	SchemePeer    = "peer"
 	SchemeSelf    = "self"
 )
