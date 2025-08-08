301eeceb4e1320d26e1e1aab4d6adfd0f46c5a3c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-08-08 14:36:00 -0700

    allocation -> scheme

---
+++ schemes.go
@@ -0,0 +1,14 @@
+package ilpaddr
+
+const (
+	SchemeGlobal  = "g"
+	SchemePrivate = "private"
+	SchemeExample = "example"
+	SchemeTest    = "test"
+	SchemeTest1   = "test1"
+	SchemeTest2   = "test2"
+	SchemeTest3   = "test3"
+	SchemeLocal   = "test1"
+	SchemePeer    = "peer"
+	SchemeSelf    = "self"
+)
